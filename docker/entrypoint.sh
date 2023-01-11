64931c0e9611bdba70f02371b0e42ffef8a91830
Andrey Erokhin <aeroalgo@gmail.com>
2023-01-11 09:21:22 +0300

    docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -e
+
+# Ожидаем запуска postgres
+dockerize -wait tcp://${PSQL_HOST}:${PSQL_PORT}
+
+# Миграция и синхронизация
+./manage.py migrate --noinput
+
+# Запуск команды
+./manage.py runserver 0.0.0.0:${APP_PORT}
