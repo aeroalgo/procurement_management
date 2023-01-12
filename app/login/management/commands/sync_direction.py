447c68bd16fcfa6b4c641def856b8d933e2d334a
Andrey Erokhin <aeroalgo@gmail.com>
2023-01-12 13:51:09 +0300

    Синхронизация направлений

---
+++ app/login/management/commands/sync_direction.py
@@ -0,0 +1,23 @@
+from app.login.models import Direction
+from app.common.logger.logg import logger
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+class Command(BaseCommand):
+    help = "Sync database direction"
+
+    def handle(self, *awgs, **kwargs):
+        for direction_key, direction_title in Direction.DIRECTIONS:
+            direction = Direction()
+            direction.key = direction_key
+            direction.title = direction_title
+            direction.save()
+            logger.info(msg=f"""
+                event=sync_permissions__handle,
+                payload__group_name={direction_title},
+                """
+                        )
+
+
